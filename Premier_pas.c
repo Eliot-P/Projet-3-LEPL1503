a2bdc6e8caa24f435b47b3ec2b31f5ef6cfd6540
covanypersel <constantinvanypersele@student.uclouvain.be>
2020-03-25 00:02:48 +0100

    Version 1 de la traduction du code python

--- Premier_pas.c
+++ Premier_pas.c
@@ -1,16 +1,18 @@
+
 /*
  Created by Constantin on 16-03-20.
 
     - Traduction du code python -
+
+Last Update: 24/03
 */
 #include <sys/fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 
-typedef struct repertoire{
+typedef struct repertoire{  //Initialiser à maximum = 0 et nbre_elem = 0
     long maximum;
     int nbre_elem;
     long *liste;
@@ -36,14 +38,14 @@ int  is_prime(long number, Repertoire_t *tab){
      */
     if ((*tab).nbre_elem == 0){   // cas du premier appel
 
-        (*tab).liste = (long *) malloc(sizeof(long));
-        //if ((int) tab == -1){ return -1;}  >> comment gérer l'erreur?
+        (*tab).liste = (long *) malloc(sizeof(((*tab).nbre_elem + 1) * sizeof(long)));
+        if ((*tab).liste == NULL){ return -1;}
 
-        (*tab).liste[0] = 2;
-        (*tab).nbre_elem = 1;
-        (*tab).maximum = 2;
+        (*tab).liste[0] = (long) 2;
+        (*tab).nbre_elem = (long) 1;
+        (*tab).maximum = (long) 2;
 
-        for (long i = 2; i <= number; i++) {
+        for (long i = 3; i <= number; i++) {
             int div = 0;
             for (long j = (*tab).liste[0]; j < (*tab).liste[(*tab).nbre_elem - 1]; j++) {
                 // On regarde si i est un multiple d'un nbre premier
@@ -61,7 +63,7 @@ int  is_prime(long number, Repertoire_t *tab){
             }
             if (div == 0) {
                 (*tab).liste = realloc((*tab).liste, sizeof(((*tab).nbre_elem + 1) * sizeof(long)));
-                //if ((int) (*tab).liste == -1) { return -1; } > comment gérer l'erreur de realloc ?
+                if ( (*tab).liste == NULL) { return -1; } //> comment gérer l'erreur de realloc ?
                 (*tab).liste[(*tab).nbre_elem++] = i;
                 (*tab).maximum = i;
             }
@@ -70,8 +72,11 @@ int  is_prime(long number, Repertoire_t *tab){
     }
     else {
         for (long i = 0; i < (*tab).nbre_elem; i++) {
+            //printf("%ld",(*tab).liste[i]);
             if ((*tab).liste[i] == number) { return 1; }
-            if (number % (*tab).liste[i] == 0) { return 0; }  // cas où number est un multiple d'un des nbre premiers
+            if (number % (*tab).liste[i] == 0) {
+                break;
+            }  // cas où number est un multiple d'un des nbre premiers
         }
 
         for (long i = (*tab).maximum; i <= number; i++){
@@ -91,8 +96,8 @@ int  is_prime(long number, Repertoire_t *tab){
                 }
             }
             if (div == 0){  //On a bien un nbre premier et donc on doit réajuster la taille de la structure pour le stocker
-                (*tab).liste = realloc((*tab).liste, sizeof(((*tab).nbre_elem +1) * sizeof(long)));
-                //if ((int) (*tab).liste == -1){ return  -1;}  >> comment gére l'erreur ?
+                (*tab).liste = realloc(& (*tab).liste, sizeof(((*tab).nbre_elem +1) * sizeof(long)));
+                if ( (*tab).liste == NULL){ return  -1;}
                 (*tab).liste[(*tab).nbre_elem++] = i;
                 (*tab).maximum = i;
             }
@@ -101,30 +106,34 @@ int  is_prime(long number, Repertoire_t *tab){
     }
 }
 
-
-char prime_divs(long number, Repertoire_t *tab){
+char *prime_divs(long number, Repertoire_t *tab){
     /*  retourne un tableau de char contenant tout les diviseurs premiers de number
      * > alloue dynamiquement de la mémoire pour créer le tableau
     */
 
-    int size = sizeof((char) number);
-    int index = 1;
-    char *arr = (char *) malloc(size);
-    if (arr == NULL){return -1;}
+    int ok = is_prime(number,tab);
+    if (ok == 1){
+        return (char *) number;
+    }
+    char *arr = (char *) malloc(sizeof((char) number +1));
+    if (arr == NULL){ return  "Erreur avec Malloc";}
+    int index = 0;
+    arr[index++] = (char) number;
+    arr[index++] = ' '; // Pour avoir un espace entre les nbre !!
 
-    for (long i = 2; i < number; i++){
-        if (is_prime(i) && is_div(number,i)){
-            char elem = (char) i;
-            size += sizeof(elem);
-            arr = (char *) realloc(arr, size);
-            if (arr == NULL){ return -1;}
-            arr[index] = elem;
+    for (long j = 0; j < (*tab).nbre_elem; j++){    // Tout nbre non premier est un multiple de nbre premiers
+        if  (is_div(number,(*tab).liste[j])){
+            arr = (char *) realloc( arr, sizeof(arr) + sizeof((char) j) +1);   // OK???
+            if (arr == NULL){ return  "Erreur avec Realloc";}
+            arr[index++] = (char) j;
+            arr[index++] = ' ';
         }
     }
-    return *arr;
+    printf("%s",arr);
+    return arr;
 }
 
-int principale(char *input, char *output_file){
+int principale(char *input, char *output_file) {
     /*
      * pré: input != NULL ; output_file != NULL  Ce sont des fichiers
      * input est un fichier qui contient un élément (int,char,float,...) par ligne
@@ -141,117 +150,128 @@ int principale(char *input, char *output_file){
      *          -6 -> erreur causée par malloc()
      *          -7 -> erreur causée par msync()
      */
-     int in = open(input, O_RDONLY);
-     if (in == -1){return -1;}
-     int out = open(output_file, O_RDWR |O_TRUNC | O_CREAT, S_IRWXU);
-     if (out == -1){return -1;}
+    int in = open(input, O_RDONLY);
+    if (in == -1) { return -1; }
+    int out = open(output_file, O_RDWR | O_TRUNC | O_CREAT, S_IRWXU);
+    if (out == -1) { return -1; }
 
-     struct stat buf;
-     int fs = fstat(in, &buf);
-     if (fs == -1){
-         close(in);
-         close(out);
-         return -5;
-     }
+    struct stat buf;
+    int fs = fstat(in, &buf);
+    if (fs == -1) {
+        close(in);
+        close(out);
+        return -5;
+    }
 
-     if (buf.st_size == 0){        //exception où le fichier est vide
-         int j = close(in);
-         if (j == -1){return -1;}
-         return close(out);
-     }
+    if (buf.st_size == 0) {        //exception où le fichier est vide
+        int j = close(in);
+        if (j == -1) { return -1; }
+        return close(out);
+    }
 
-     long *tabin = (long *) mmap(NULL,buf.st_size,PROT_READ,MAP_SHARED,in,0);
-     if (*tabin == -1){
-         close(in);
-         close(out);
-         return -2;
-     }
-     int size = 0;
-     for (long i = 0; i < buf.st_size/sizeof(long); i++){
-         long number = tabin[i];
+    long *tabin = (long *) mmap(NULL, buf.st_size, PROT_READ, MAP_SHARED, in, 0);
+    if (*tabin == -1) {
+        close(in);
+        close(out);
+        return -2;
+    }
+    int size = 0;
+    Repertoire_t *rep = (Repertoire_t *) malloc(sizeof(struct repertoire));
+    if (rep == NULL) {
+        munmap(tabin, buf.st_size);
+        close(in);
+        close(out);
+        return -6;
+    }
 
-         if (number < 2){
-             char *message = (char *) malloc(sizeof("Erreur: nombre inférieur à 2 !\n")); // +1 pr le \0 ??
-             if (message == NULL){ return -6;}
-             message =  "Erreur: nombre inférieur à 2 !\n";
+    rep->nbre_elem = 0;
+    rep->maximum = 0;
 
-             size += sizeof(message);
-             int ff = ftruncate(out,size);      // On modifie la taille du fichier pr pouvoir écrire dedans
-             if (ff == -1){
-                 munmap(tabin,buf.st_size);
-                 close(in);
-                 close(out);
-                 printf("Mémoire insufisante dans le fichier de sortie");
-                 return -5;
-             }
-             char *tabout = (char *) mmap(NULL,size,PROT_WRITE|PROT_READ,MAP_SHARED,out,0);
-             if ((int) *tabout == -1){
-                 munmap(tabin,size);
-                 close(in);
-                 close(out);
-                 return -2;
-             }
-             memcpy(tabout, message, sizeof(&message));
+    for (long i = 0; i < buf.st_size / sizeof(long); i++) {
+        long number = tabin[i];
 
-             int s = msync(tabout,size,MS_SYNC);
-             free(message);     // Libère la mémoire
+        if (number < 2) {
+            char *message = (char *) malloc(sizeof("Erreur: nombre inférieur à 2 !\n")); // +1 pr le \0 ??
+            if (message == NULL) { return -6; }
+            message = "Erreur: nombre inférieur à 2 !\n";
 
-             if (s == -1){
-                 munmap(tabin,buf.st_size);
-                 munmap(tabout,buf.st_size);
-                 close(in);
-                 close(out);
-                 return -7;
-             }
-             int f = munmap(tabout,size);
-             if (f == -1){
-                 munmap(tabin,buf.st_size);
-                 close(in);
-                 close(out);
-                 return -3;
-             }
-         }
+            size += sizeof(message);
+            int ff = ftruncate(out, size);      // On modifie la taille du fichier pr pouvoir écrire dedans
+            if (ff == -1) {
+                munmap(tabin, buf.st_size);
+                close(in);
+                close(out);
+                printf("Mémoire insufisante dans le fichier de sortie");
+                return -5;
+            }
+            char *tabout = (char *) mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, out, 0);
+            if ((int) *tabout == -1) {
+                munmap(tabin, size);
+                close(in);
+                close(out);
+                return -2;
+            }
+            memcpy(tabout, message, sizeof(&message));
 
-         else{
-             char message = prime_divs(number); // Est-ce que ca fonctionne de catser un tableau de int en char ????
-             size += sizeof(message);
+            int s = msync(tabout, size, MS_SYNC);
+            free(message);     // Libère la mémoire
 
-             char *tabout = (char *) mmap(NULL,size,PROT_WRITE|PROT_READ,MAP_SHARED,out,0);
-             if ((int) *tabout == -1){
-                 munmap(tabin,size);
-                 close(in);
-                 close(out);
-                 return -2;
-             }
-             memcpy(tabout, &message, sizeof(message));
-             int s = msync(tabout,size,MS_SYNC);
-             free(&message);     // Libère la mémoire
+            if (s == -1) {
+                munmap(tabin, buf.st_size);
+                munmap(tabout, buf.st_size);
+                close(in);
+                close(out);
+                return -7;
+            }
+            int f = munmap(tabout, size);
+            if (f == -1) {
+                munmap(tabin, buf.st_size);
+                close(in);
+                close(out);
+                return -3;
+            }
+        } else {
 
-             if (s == -1){
-                 munmap(tabin,buf.st_size);
-                 munmap(tabout,buf.st_size);
-                 close(in);
-                 close(out);
-                 return -7;
-             }
-             int f = munmap(tabout,size);
-             if (f == -1){
-                 munmap(tabin,buf.st_size);
-                 close(in);
-                 close(out);
-                 return -3;
-             }
-             }
-          }
+            char *message = prime_divs(number, rep);
+            size += sizeof(*message);  // !!!! Est-ce que ca fait bien référence au contenu de message ?!!!!!!
 
-     int m = munmap(tabin,buf.st_size);
-     if (m== -1){
-         close(in);
-         close(out);
-         return -3;
-     }
-     int j = close(in);
-     int i = close(out);
-     if ((i== -1)|(j==-1)){return -4;}
-     return 0;
+            char *tabout = (char *) mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, out, 0);
+            if ((int) *tabout == -1) {
+                munmap(tabin, size);
+                close(in);
+                close(out);
+                return -2;
+            }
+            memcpy(tabout, &message, sizeof(*message));
+            int s = msync(tabout, size, MS_SYNC);
+
+            free(&message);     // Libère la mémoire
+
+            if (s == -1) {
+                munmap(tabin, buf.st_size);
+                munmap(tabout, buf.st_size);
+                close(in);
+                close(out);
+                return -7;
+            }
+            int f = munmap(tabout, size);
+            if (f == -1) {
+                munmap(tabin, buf.st_size);
+                close(in);
+                close(out);
+                return -3;
+            }
+        }
+    }
+
+    int m = munmap(tabin, buf.st_size);
+    if (m == -1) {
+        close(in);
+        close(out);
+        return -3;
+    }
+    int j = close(in);
+    int i = close(out);
+    if ((i == -1) | (j == -1)) { return -4; }
+    return 0;
 }
