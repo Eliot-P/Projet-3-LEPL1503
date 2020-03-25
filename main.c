5f1c7d0fc6f3524eb8699b06e3e84f48746eed80
covanypersel <constantinvanypersele@student.uclouvain.be>
2020-03-25 13:19:31 +0100

    n

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include "Premier_pas.h"
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-	if((test())==0)
-		return EXIT_SUCCESS;
-	else
-		return test();
+int main(int argc, char *argv[]){
+    Repertoire_t *rep = (Repertoire_t *) malloc(struct repertoire);
+    rep->maximum = 0;
+    rep->nbre_elem = 0;
+    printf(is_prime(300000,rep));
+	return 0;
 }
