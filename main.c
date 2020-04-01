17b57f40ea45cd4eafe96be0a6a575cb5367e02d
elipeeters <eliot.peeters@student.uclouvain.be>
2020-04-01 12:32:31 +0200

    fonction temps dans main

--- main.c
+++ main.c
@@ -8,24 +8,11 @@
 
 
 int main() {
-    /*
-    Repertoire_t *a = (Repertoire_t *) malloc(sizeof(struct repertoire));
-    a->liste = (long *) malloc(sizeof(long) *4);
-    a->liste[0] = (long) 2; a->liste[1] = (long) 3; a->liste[2] = (long) 5; a->liste[3] = (long) 7;
-    a->maximum = 7;
-    a->nbre_elem = 4;
-
-    time_t depart = time(NULL);
-    is_prime(2000000,a);
-    time_t intermediaire = time(NULL);
-    //is_prime(1000000,a);
-    time_t fin = time(NULL);
-    printf("Nombre d'élements: %i\nPlus grand nombre premier: %ld\n",a->nbre_elem,a->maximum);
-    printf("Temps mis pour 2 000 000 : %li [s]\nTemps mis pour 1 000 000 : %li [s]",(intermediaire-depart),(fin-intermediaire));
-    /*
-    char *mess = prime_divs(100,a);
-    printf("%c",*mess);
-    */
+    clock_t t;
+    t = clock();
     test();
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("%f\n", time_taken);
     return 0;
 }
