be97e9ec43715443a88dcfcea615d072e4a4af64
elipeeters <eliot.peeters@student.uclouvain.be>
2020-04-11 16:44:18 +0200

    test_lib finie mais certains tests ne passent pas

--- test_lib.c
+++ test_lib.c
@@ -1,19 +1,13 @@
 #include "Prime.h"
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
-#include <sys/fcntl.h>
-#include <sys/fcntl.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
-void test_is_div(){
+int test_is_div(){
     CU_ASSERT_TRUE(is_div(2, 2));
     CU_ASSERT_TRUE(is_div(8, 2));
     CU_ASSERT_FALSE(is_div(19, 2));
     CU_ASSERT_FALSE(is_div(19, 3));
+    return 0;
 
 }
 int test_is_prime(){
@@ -28,7 +22,7 @@ int test_is_prime(){
     CU_ASSERT_TRUE(is_prime(13,test_array));
     
     CU_ASSERT_FALSE(is_prime(10, test_array));
-    int prime_test_array[] = {1, 2, 3, 5, 7, 11, 1223, 51991, 802499};
+    int prime_test_array[] = {2, 3, 5, 7, 11, 1223, 51991, 802499};
     for(int i = 0; i < 9; i++){
         CU_ASSERT_TRUE(is_prime(prime_test_array[i], test_array));
     }
@@ -38,32 +32,43 @@ int test_is_prime(){
     }   
     return 0;
 }
-    /*
+    
 
+int test_prime_divs(){
+    Repertoire_t *test_array = (Repertoire_t *) malloc(sizeof(struct repertoire));
+    if (test_array == NULL){ return -1;}
+    test_array->liste = (long *) malloc(sizeof(long) *4);
+    if (test_array->liste == NULL){return -1;}
+    test_array->liste[0] = (long) 2;
+    test_array->liste[1] = (long) 3;
+    test_array->liste[2] = (long) 5;
+    test_array->liste[3] = (long) 7;
 
-
-    //test prime_divs()
     for(int i = 0; i < 9; i++){
-        CU_ASSERT_EQUAL(prime_divs(prime[i], a), prime[i]);
+        int prime_test_array[] = {2, 3, 5, 7, 11, 1223, 51991, 802499};
+        CU_ASSERT_EQUAL(prime_divs(prime_test_array[i], test_array), prime_test_array[i]);
     }
-    CU_ASSERT_EQUAL(prime_divs(4, a), 2);
+    CU_ASSERT_EQUAL(prime_divs(4, test_array), 2);
     int ret1[] = {2, 3, 5};
     for(int i = 0; i < 3; i++){
-        CU_ASSERT_EQUAL((prime_divs(4, a) + i), ret1[i]);
+        CU_ASSERT_EQUAL((prime_divs(4, test_array) + i), ret1[i]);
     }
     for(int i = 0; i < 3; i++){
-        CU_ASSERT_EQUAL((prime_divs(4, a) + i), ret1[i]);
+        CU_ASSERT_EQUAL((prime_divs(4, test_array) + i), ret1[i]);
     }
     int ret2[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293};
     for(int i = 0; i < 62 ; i++){
-        CU_ASSERT_EQUAL((prime_divs(4, a) + i), ret2[i]);
+        CU_ASSERT_EQUAL((prime_divs(4, test_array) + i), ret2[i]);
     }
-    */
+    return 0;
+}
+
 int main (){
     CU_initialize_registry();
     CU_pSuite suite = CU_add_suite("Prime test", 0, 0);
     CU_add_test(suite, "is_div Test", test_is_div);
     CU_add_test(suite,"is_prime_test",test_is_prime);
+    CU_add_test(suite,"prime_divs_test",test_prime_divs);
     CU_basic_run_tests();
     CU_cleanup_registry();
     return 0;
