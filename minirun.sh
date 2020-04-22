4bae7e2a51d6827fe87fd260ad5193a2719a1a07
elipeeters <eliot.peeters@student.uclouvain.be>
2020-04-22 15:14:39 +0200

    git auto command

--- minirun.sh
+++ minirun.sh
@@ -5,4 +5,4 @@ gcc -c Prime.c -w -std=gnu9x
 gcc -c Prime_thread.c -w -std=gnu9x -lpthread
 gcc -c miniscript.c -w -std=gnu9x
 gcc -o minirun Prime.o Prime_thread.o  miniscript.o -w -std=gnu9x -lpthread
-./minirun
+gcc -o ui Prime_thread.o  miniscript.o -w -std=gnu9x -lpthread
