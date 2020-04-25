fa0e5e5225afac2ff2e2d91e2ffbf88bf57d9726
elipeeters <eliot.peeters@student.uclouvain.be>
2020-04-25 17:43:41 +0200

    test de la fonction thread : test is prime

--- test_lib.c
+++ test_lib.c
@@ -12,66 +12,30 @@ int test_is_div(){
     return 0;
 
 }
+
 int test_is_prime(){
-    Repertoire_t_th *test_array = (Repertoire_t_th *) malloc(sizeof(struct repertoire));
-    if (test_array == NULL){ return -1;}
-    test_array->liste = (long *) malloc(sizeof(long) *4);
-    if (test_array->liste == NULL){return -1;}
-    test_array->liste[0] = (long) 2;
-    test_array->liste[1] = (long) 3;
-    test_array->liste[2] = (long) 5;
-    test_array->liste[3] = (long) 7;
-    CU_ASSERT_TRUE(is_prime(13,test_array));
-    CU_ASSERT_FALSE(is_prime(10, test_array));
-    long prime_test_array[] = {2, 3, 5, 7, 11, 1223, 51991, 802499};
-    for(int i = 0; i < 7; i++){
-        CU_ASSERT_TRUE(is_prime(prime_test_array[i], test_array));
-    }
-    long not_prime_test_array[] = {4, 10, 303, 18252};
-    for(int i = 0; i < 3; i++){
-        CU_ASSERT_FALSE(is_prime(not_prime_test_array[i], test_array));
-    }   
-    return 0;
-}
-    
-int test_is_prime(){
-     Repertoire_t_th *a = (Repertoire_t_th *) malloc(sizeof(struct repertoire));
-    if (a == NULL){ return -1;}
+    Repertoire_t_th *test_struct = (Repertoire_t_th *) malloc(sizeof(struct repertoire_th));
+    if (test_struct == NULL){ return -1;}
     pthread_mutex_t mutex;
-    unsigned long long p[] = {2, 13, 97, 997, 3456863};
-    for (int i = 0; i < 5; i++){
-        CU_ASSERT_TRUE(is_prime(p[i],a));//,&mutex));
+    unsigned long long array_of_prime[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113};
+    for (int i = 0; i < 30; i++){
+        CU_ASSERT_TRUE(is_prime(array_of_prime[i],test_struct,&mutex));
+        CU_ASSERT_EQUAL(test_struct->liste[i], array_of_prime[i]);
+        CU_ASSERT_EQUAL(test_struct->nbre_elem, i+1);
+    }
+    unsigned long long array_of_not_prime[] = {1,4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,27,28,30,32,33,34,35,36,38,39,40,42,44};
+    int nbr_no_change = test_struct->nbre_elem;
+    for (int i = 0; i < 30; i++){
+        CU_ASSERT_FALSE(is_prime(array_of_not_prime,test_struct,&mutex));
+        CU_ASSERT_EQUAL(test_struct->liste[i],array_of_prime[i]);
+        CU_ASSERT_EQUAL(test_struct->nbre_elem,nbr_no_change);
     }
     return 0;
 }
 
 
 int test_prime_divs(){
-    Repertoire_t_th *test_array = (Repertoire_t_th *) malloc(sizeof(struct repertoire));
-    if (test_array == NULL){ return -1;}
-    test_array->liste = (long *) malloc(sizeof(long) *4);
-    if (test_array->liste == NULL){return -1;}
-    test_array->liste[0] = (long) 2;
-    test_array->liste[1] = (long) 3;
-    test_array->liste[2] = (long) 5;
-    test_array->liste[3] = (long) 7;
-
-    for(int i = 0; i < 7; i++){
-        int prime_test_array[] = {2, 3, 5, 7, 11, 1223, 51991, 802499};
-        CU_ASSERT_EQUAL(prime_divs(prime_test_array[i], test_array), prime_test_array[i]);
-    }
-    CU_ASSERT_EQUAL(prime_divs(4, test_array), 2);
-    int ret1[] = {2, 3, 5};
-    for(int i = 0; i < 3; i++){
-        CU_ASSERT_EQUAL((prime_divs(4, test_array) + i), ret1[i]);
-    }
-    for(int i = 0; i < 3; i++){
-        CU_ASSERT_EQUAL((prime_divs(4, test_array) + i), ret1[i]);
-    }
-    int ret2[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293};
-    for(int i = 0; i < (sizeof(ret2)/sizeof(ret2[0])) ; i++){
-        CU_ASSERT_EQUAL((prime_divs(4, test_array) + i), ret2[i]);
-    }
+    
     return 0;
 }
 
@@ -82,7 +46,6 @@ int main (){
     CU_add_test(suite, "is_div Test", test_is_div);
     CU_add_test(suite,"is_prime_test",test_is_prime);
     CU_add_test(suite,"prime_divs_test",test_prime_divs);
-    CU_add_test(suite,"test_is_prime",test_is_prime);
     CU_basic_run_tests();
     CU_cleanup_registry();
     return 0;
