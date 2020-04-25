05e543bfdd55d60cd127ab27a0cf516e591d45f2
elipeeters <eliot.peeters@student.uclouvain.be>
2020-04-25 18:33:47 +0200

    test unitaire finit pour les 3 fonctions de bases

--- test_lib.c
+++ test_lib.c
@@ -10,7 +10,6 @@ int test_is_div(){
     CU_ASSERT_FALSE(is_div(19, 2));
     CU_ASSERT_FALSE(is_div(19, 3));
     return 0;
-
 }
 
 int test_is_prime(){
@@ -35,7 +34,22 @@ int test_is_prime(){
 
 
 int test_prime_divs(){
-    
+    Repertoire_t_th *test_struct = (Repertoire_t_th *) malloc(sizeof(struct repertoire_th));
+    if (test_struct == NULL){ return -1;}
+    pthread_mutex_t mutex;
+    unsigned long long array_of_prime[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113};
+    unsigned long long array_of_not_prime[] = {1,4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,27,28,30,32,33,34,35,36,38,39,40,42,44};
+    for (int i = 0; i < 30; i++){
+        CU_ASSERT_EQUAL(prime_divs(array_of_prime[i],test_struct,&mutex)->liste[0],array_of_prime[i]);
+        CU_ASSERT_EQUAL(test_struct->nbre_elem, i+1);
+        }
+    int nbr_no_change = test_struct->nbre_elem;
+    unsigned long long list_no_change = test_struct->liste[0];
+    for (int i = 0; i < 30; i++){
+        CU_ASSERT_NOT_EQUAL(prime_divs(array_of_prime[i],test_struct,&mutex)->liste[0],array_of_not_prime[i]);
+        CU_ASSERT_EQUAL(test_struct->liste[0],list_no_change);
+        CU_ASSERT_EQUAL(test_struct->nbre_elem,nbr_no_change);
+    }
     return 0;
 }
 
