5f9706356588306f99759373fa0cc3b4b4dc3c71
elipeeters <eliot.peeters@student.uclouvain.be>
2020-05-14 15:51:36 +0200

    ..

--- SCRIPTS/Shell_Prime.sh
+++ SCRIPTS/Shell_Prime.sh
@@ -2,6 +2,7 @@ echo "Entrer le mode d'execution"
 echo "1) Unit test"
 echo "2) Debug"
 echo "3) Auto prime"
+echo "4) Statistique Git"
 read execution_mode
 
 if [ $execution_mode -eq 1 ]; then
@@ -34,9 +35,9 @@ fi
 if [ $execution_mode -eq 3 ]; then
     echo "Execution de fact"
     make clean > /dev/null
-    gcc -o ../OBJECT/fact.o -c ../SRC/fact.c -w -std=gnu9x -lpthread > /dev/null
-    gcc -o ../OBJECT/main.o -c ../SRC/main.c -w -std=gnu9x > /dev/null
-    gcc -o fact ../OBJECT/main.o ../OBJECT/fact.o  -w -std=gnu9x -lpthread > /dev/null
+    gcc -o ../TEMP/fact.o -c ../SRC/fact.c -w -std=gnu9x -lpthread > /dev/null
+    gcc -o ../TEMP/main.o -c ../SRC/main.c -w -std=gnu9x > /dev/null
+    gcc -o fact ../TEMP/main.o ../TEMP/fact.o  -w -std=gnu9x -lpthread > /dev/null
     echo "Nombre de thread ?"
     read N_thread
     ./fact -N $N_thread -q ../IN-OUT/Input.txt ../IN-OUT/Output_thread.txt 
@@ -44,4 +45,13 @@ if [ $execution_mode -eq 3 ]; then
     make clean > /dev/null
     rm -rf ../IN-OUT/Output_thread.txt > /dev/null
     rm -rf fact > /dev/null
+fi
+
+if [ $execution_mode -eq 4 ]; then
+    echo "Statistiques git à la date du :">../OTHER/stats.txt
+	date >>../OTHER/stats.txt
+	gitinspector >> ../OTHER/stats.txt
+	echo "\n" >> ../OTHER/stats.txt
+	echo "nombres de commit" >>../OTHER/stats.txt
+	git shortlog -sn >> ../OTHER/stats.txt
 fi
