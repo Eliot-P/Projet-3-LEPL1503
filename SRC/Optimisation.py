4d72169869b0351b9fa5d817d0d0cfffd8eb0b26
elipeeters <eliot.peeters@student.uclouvain.be>
2020-05-14 13:20:54 +0200

    changement de nom pour respecter les conventions

---
+++ SRC/Optimisation.py
@@ -0,0 +1,112 @@
+import os
+from numpy import *
+import matplotlib.pyplot as plt
+import cpuinfo
+
+plt.style.use('ggplot')
+
+def grapher_time(array,n_error):
+    n = 1
+    ax = plt.subplot()
+    arr = asarray(array)
+    mean_arr = []
+    for n_thread in arr : 
+        stand = around(std(n_thread),2)
+        maxi = around(amax(n_thread),2)
+        mini = around(amin(n_thread),2)
+        moy = around(mean(n_thread),2)
+        ax.plot(n,maxi,'go',)
+        ax.plot(n,mini,'go')
+        ax.plot([n,n],[mini,maxi],'g')
+        ax.plot(n,moy,'bo')
+        ax.text(n + 0.09,moy,"$\sigma={}$\nmoy={}\nmax={}\nmin={}".format(stand,moy,maxi,mini),bbox=dict(facecolor='wheat', alpha=0.7))
+        mean_arr.append(moy)
+        n+=1
+    mean_numpy_arr = asarray(mean_arr)
+    ax.plot(range(1,n),mean_numpy_arr,'b--')
+    ax.axhline(amin(mean_numpy_arr))
+    ax.set_xlabel('Number of thread [N]')
+    ax.set_ylabel('Time [ms]')
+    ax.text(0.05, 0.1, '{} ERREURS'.format(n_error), fontsize=10, transform=plt.gcf().transFigure)
+    ax.set_title("Execution de fact avec l'exemple d'input sur un processeur " + cpuinfo.get_cpu_info()['brand'].split("w/")[0] ,pad=30)
+    plt.draw()
+    plt.savefig('../OTHER/Time_of_execution.png',bbox_inches='tight')
+    plt.close()
+    return mean_arr
+
+def grapher_memory(array,n_error):
+    n = 1
+    ax2 = plt.subplot()
+    arr = asarray(array)
+    mean_arr = []
+    for n_thread in arr : 
+        stand = around(std(n_thread),2)
+        maxi = around(amax(n_thread),2)
+        mini = around(amin(n_thread),2)
+        moy = around(mean(n_thread),2)
+        ax2.plot(n,maxi,'go',)
+        ax2.plot(n,mini,'go')
+        ax2.plot([n,n],[mini,maxi],'g')
+        ax2.plot(n,moy,'bo')
+        ax2.text(n + 0.09,moy,"$\sigma={}$\nmoy={}\nmax={}\nmin={}".format(stand,moy,maxi,mini),bbox=dict(facecolor='wheat', alpha=0.7))
+        mean_arr.append(moy)
+        n+=1
+    mean_numpy_arr = asarray(mean_arr)
+    ax2.plot(range(1,n),mean_numpy_arr,'b--')
+    ax2.axhline(amin(mean_numpy_arr))
+    ax2.set_xlabel('Number of thread [N]')
+    ax2.set_ylabel('Memory usage [kB]')
+    ax2.text(0.05, 0.1, '{} ERREURS'.format(n_error), fontsize=10, transform=plt.gcf().transFigure)
+    ax2.set_title("Execution de fact avec l'exemple d'input sur un processeur " + cpuinfo.get_cpu_info()['brand'].split("w/")[0] ,pad=30)
+    plt.draw()
+    plt.savefig('../OTHER/Memory_usage.png',bbox_inches='tight')
+    plt.close()
+
+def grapher_Amdahl(mean_arr) :
+    ax = plt.subplot()
+    Number_of_thread = arange(1,len(mean_arr)+1)
+    Fraction_of_thread = 0.90
+    Gain =  1/((1-Fraction_of_thread)+(Fraction_of_thread/Number_of_thread))
+    ax.set_xlabel("Number of thread")
+    ax.set_ylabel("Speed Up")
+    ax.set_title("Amdahl's Law",pad=30)
+    ax.plot(Number_of_thread,mean_arr/mean_arr[0],'o--',label="Real World")
+    ax.plot(Number_of_thread,Gain,"-",label="Amdahl's Law")
+    plt.draw()
+    plt.savefig('../OTHER/Amdahl.png',bbox_inches='tight')
+    plt.close()
+
+def exec (Number_of_thread) : 
+    execution = os.popen('./../SCRIPTS/fact -N {} -q ../IN-OUT/Input.txt ../IN-OUT/Output.txt'.format(str(Number_of_thread))) #écrit dans le terminal
+    time_taken_raw = execution.readlines() #lit ce que l'execution écris dans le terminal
+    print(time_taken_raw[3].split(" ")[3])
+    print(time_taken_raw[2].split(" ")[3])
+    try :
+        time_taken = float(time_taken_raw[3].split(" ")[3])*1000#transforme en float et retire le "\n"
+        memory_taken = float(time_taken_raw[2].split(" ")[3])
+    except:
+        time_taken = -1
+    return time_taken,memory_taken
+
+def main (number_of_exec,max_number_of_thread):
+    big_array_of_time = []
+    big_array_of_memory = []
+    n_error = 0
+    for n in range (max_number_of_thread):
+        array_of_time_for_n_thread = []
+        array_of_memory_for_n_thread = []
+        for i in range (number_of_exec):
+            time_taken,memory_taken = exec(max_number_of_thread)
+            print("i={}".format(i))
+            if time_taken != -1 :
+                array_of_time_for_n_thread.append(time_taken)
+                array_of_memory_for_n_thread.append(memory_taken)
+            else : 
+                n_error += 1
+        big_array_of_time.append(array_of_time_for_n_thread)
+        big_array_of_memory.append(array_of_memory_for_n_thread)
+        print("n={}".format(n))
+    mean_arr = grapher_time(big_array_of_time,n_error)
+    grapher_memory(big_array_of_memory,n_error)
+    grapher_Amdahl(mean_arr)
+main(4,8)
---
+++ SRC/test.c
@@ -0,0 +1,179 @@
+#include "fact.h"
+#include <pthread.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+
+int test_is_div(){
+    // Test de la fonction is_div
+    CU_ASSERT_TRUE(is_div(2, 2));
+    CU_ASSERT_TRUE(is_div(8, 2));
+    CU_ASSERT_FALSE(is_div(19, 2));
+    CU_ASSERT_FALSE(is_div(19, 3));
+    return 0;
+}
+
+int test_prime_divs_opti(){
+    //Test de la fonction prime_divs
+    //D'abord on initialise une structure test afin que la fonction puisse effectuer les changements dessus
+    Repertoire_t_th *test_struct = (Repertoire_t_th *) malloc(sizeof(struct repertoire_th));
+    if (test_struct == NULL){ return -1;}
+    //On initialise aussi un thread unique pour la fonction
+    pthread_mutex_t mutex;
+    //Liste des 30 premiers nombres premiers
+    unsigned long long array_of_prime[] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113};
+    //Liste des 30 premiers nombres non-premiers
+    unsigned long long array_of_not_prime[] = {1,4,6,8,9,10,12,14,15,16,18,20,21,22,24,25,26,27,28,30,32,33,34,35,36,38,39,40,42,44};
+    for (int i = 0; i < 30; i++){
+        //D'abord on vérifie que chaque nombre premier est bien ajouté dans la liste
+        // !! cette liste se comporte comme une liste FIFO (first in first out) donc le dernier élément ajouté se trouve à l'index 0
+        CU_ASSERT_EQUAL(prime_divs(array_of_prime[i],test_struct,&mutex)->liste[0],array_of_prime[i]);
+        //Enfin on vérifie aussi que le nombre d'élément est aussi actualisé dans la structure
+        CU_ASSERT_EQUAL(test_struct->nbre_elem, i+1);
+        }
+    //On fixe le nombre d'élément dans la liste
+    int nbr_no_change = test_struct->nbre_elem;
+    //On fixe le dernier élément ajouté dans la liste
+    unsigned long long list_no_change = test_struct->liste[0];
+    for (int i = 0; i < 30; i++){
+        //D'abord on vérifie que le nombre non-premier n'a pas été ajouté dans la liste (donc ne se trouve pas à l'index 0)
+        CU_ASSERT_NOT_EQUAL(prime_divs(array_of_prime[i],test_struct,&mutex)->liste[0],array_of_not_prime[i]);
+        //Ensuite on vérifie que l'élément à l'index 0 est toujours le même
+        CU_ASSERT_EQUAL(test_struct->liste[0],list_no_change);
+        //Enfin on vérifie que le nombre d'élément dans la liste est toujours le même
+        CU_ASSERT_EQUAL(test_struct->nbre_elem,nbr_no_change);
+    }
+    free(test_struct);
+    return 0;
+}
+
+int test_tricky_cases(){
+    Repertoire_t_th *test_struct = (Repertoire_t_th *) malloc(sizeof(struct repertoire_th));
+    if (test_struct == NULL){ return -1;}
+    pthread_mutex_t mutex;
+    unsigned long long array_of_tricky_number[] = {0};
+    CU_ASSERT_FALSE(is_div(1,0));
+    CU_ASSERT_TRUE(is_div(0,1));
+    for (int i = 0; i < 1; i++){
+        CU_ASSERT_EQUAL(prime_divs(array_of_tricky_number[i],test_struct,&mutex)->liste[0],array_of_tricky_number[i]);
+        CU_ASSERT_EQUAL(test_struct->nbre_elem, i+1);
+        CU_ASSERT_FALSE(is_prime(array_of_tricky_number[i],test_struct,&mutex));
+    }
+    free(test_struct);
+    return 0;
+}
+
+int test_output(){
+    //fichier d'entrée
+    FILE *input_test= fopen("./fichiers_test/input_test.txt","r");
+    if (input_test == NULL){return -1;}
+    //fichier à comparer avec la sortie
+    FILE *output_test= fopen("./fichiers_test/output_test.txt","r");
+    if (output_test == NULL){return -1;}
+    //création du fichier output
+    principale(4, "./fichiers_test/input_test.txt", "./fichiers_test/output.txt");
+    FILE *output= fopen("./fichiers_test/output.txt","r");
+    if (output == NULL){return -1;}
+    char line[30];
+    char line_test[30];
+    //comparaison entre les deux fichiers
+    while(fgets(line, 30, output) != NULL){
+        fgets(line_test, 30, output_test);
+        CU_ASSERT_EQUAL(*line, *line_test);
+    }
+    return 0;
+}
+
+int AppendNumber_test(){
+    //création d'un répertoire pour le test
+    Repertoire_t_th *rep = (Repertoire_t_th *) malloc(sizeof(struct repertoire_th));
+    if (rep == NULL){
+        return -1;
+    }
+    rep->liste = (unsigned long long *) malloc(sizeof(unsigned long long));
+    if (rep->liste == NULL){
+        return  -1;
+    }
+    rep->nbre_elem = 0;
+
+    for(int i = 0; i < 100; i++){
+        int number = rand() % 1000; //création d'un nombre random entre 0 et 999
+        int err = AppendNumber(number, rep); // ajout de number
+        if(err == -1){
+            return -1;
+        }
+        CU_ASSERT_EQUAL(number, rep->liste[i]) //vérifie si number à été ajouté
+    }
+    return 0;
+}
+
+int putNumber_test(){
+    for(int j = 0; j<10; j++) {
+        //création d'un entrepot pour le test
+        Entrepot_Th *tab = (Entrepot_Th *) malloc(sizeof(struct entrepot));
+        if (tab == NULL) {
+            return -1;
+        }
+        tab->buffer = (Repertoire_t_th *) malloc(sizeof(struct repertoire_th) * 8);
+        if (tab->buffer == NULL) {
+            return -1;
+        }
+        tab->putindex = 0;
+        tab->takeindex = 0;
+        tab->size = 8;
+        tab->nbre = 0;
+
+
+        for (int i = 0; i < 7; i++) {
+            int number = rand() % 1000; //création d'un nombre random entre 0 et 999
+            putNumber(tab, number); // ajout de number
+
+            CU_ASSERT_EQUAL((tab->buffer[i]).liste[0], number); //vérifie si number à été ajouté
+            CU_ASSERT_EQUAL(1, (tab->buffer[i]).nbre_elem); //verifie que le nombre d'élément à été augmenté à 1
+            CU_ASSERT_EQUAL(i + 1, tab->nbre); //verifie que le nbre augmente à chaque ajout
+            //free((tab->buffer[tab->putindex]).liste);
+        }
+    }
+    return 0;
+
+}
+
+int test_big_number(){
+    //fichier d'entrée
+    FILE *input_test= fopen("./fichiers_test/big_numbers_in.txt","r");
+    if (input_test == NULL){return -1;}
+    //fichier à comparer avec la sortie
+    FILE *output_test= fopen("./fichiers_test/big_numbers_out.txt","r");
+    if (output_test == NULL){return -1;}
+    //création du fichier output
+    principale(4, "./fichiers_test/big_numbers_in.txt", "./fichiers_test/output.txt");
+    FILE *output= fopen("./fichiers_test/output.txt","r");
+    if (output == NULL){return -1;}
+    char line[30];
+    char line_test[30];
+    //comparaison entre les deux fichiers
+    while(fgets(line, 30, output) != NULL){
+        fgets(line_test, 30, output_test);
+        CU_ASSERT_EQUAL(*line, *line_test);
+    }
+    return 0;
+}
+
+int main(){
+    CU_initialize_registry();
+    CU_pSuite suite = CU_add_suite("Prime test", 0, 0);
+    CU_add_test(suite,"tricky_cases",test_tricky_cases);
+    CU_add_test(suite, "is_div Test", test_is_div);
+    CU_add_test(suite,"is_prime_test",test_is_prime);
+    CU_add_test(suite,"prime_divs_test",test_prime_divs_opti);
+    CU_add_test(suite,"test_output", test_output);
+    CU_add_test(suite,"test_big_number", test_big_number);
+    CU_add_test(suite,"AppendNumber_test", AppendNumber_test);
+    CU_add_test(suite,"putNumber_test", putNumber_test);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return 0;
+}
+
